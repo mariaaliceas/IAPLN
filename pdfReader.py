ddaacca559c1df90a950c0d21edfd9da57c1478f
giovanniguilioti <gsguilioti@gmail.com>
2024-02-28 21:06:22 -0300

    references aside

--- pdfReader.py
+++ pdfReader.py
@@ -8,7 +8,9 @@ from nltk.corpus import stopwords
 from nltk.tokenize import word_tokenize, sent_tokenize, RegexpTokenizer
 from collections import Counter
 
-pdf_file = open("download.pdf", 'rb')
+tratar_texto = True
+
+pdf_file = open("articles/1.pdf", 'rb')
 
 pdf_reader = PyPDF2.PdfReader(pdf_file)
 text = ''
@@ -18,62 +20,48 @@ for i in range(len(pdf_reader.pages)):
     page = pdf_reader.pages[i]
     text += page.extract_text()
 
-'''
-    tratar texto limpo
-    retirar as referencias
-'''
-
+text = text.lower()
 words = tokenizer.tokenize(text)
 texto = []
 references = []
+bag_of_words = []
+most_commons = []
 reference = False
-for word in words:
-    if word == 'REFERENCES':
+
+for i, word in enumerate(words):
+    if (word == 'r' and words[i+1] == 'eferences') or (word == 'references' and words[i+1] == '1'):
         reference = True
     if reference:
-        if word == 'Authorized':
-            references.pop()
-            break
-        references.append(word)
-    else:
-        texto.append(word.lower())
-    
-bag_of_words = Counter(texto).most_common(10)
-#print(texto)
-print(references)
-print(bag_of_words)
-
-'''
-    stop words, lematizacao, stemming
-
-'''
-text = text.lower()
-
-stop_words = stopwords.words('english')
-# print(stop_words)
-
-palavras = tokenizer.tokenize(text)
-# print(words)
-
-texto_limpo = []
-references = []
-reference = False
-
-for word in palavras:
-    if (word not in stop_words):
-        texto_limpo.append(word)
-        if(word == 'references'):
-            reference = True
-    elif (word not in stop_words) and reference:
         if word == 'authorized':
             references.pop()
             break
-        references.append(word)
-    
-# print(texto_limpo)
-# print(references)
+        references.append(word.lower())
+    else:
+        texto.append(word.lower())
 
-porter = nltk.PorterStemmer()
-counts = [porter.stem(w) for w in texto_limpo]
+if not tratar_texto:
+    True
+else:
+    stop_words = stopwords.words('english')
 
-print(Counter(counts).most_common(10))
+    for word in words:
+        if (word == 'r' and words[i+1] == 'eferences') or (word == 'references' and words[i+1] == '1'):
+                reference = True
+        if (word not in stop_words) and not reference:
+            texto.append(word)
+        elif (word not in stop_words) and reference:
+            if word == 'authorized':
+                references.pop()
+                break
+            references.append(word)
+
+    porter = nltk.PorterStemmer()
+    stemm_words = [porter.stem(w) for w in texto]
+    # print(stemm_words)
+
+bag_of_words = Counter(texto)
+most_commons = bag_of_words.most_common(10)
+print(texto)
+#print(references)
+#print(bag_of_words)
+#print(most_commons)
