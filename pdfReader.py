d39e82baed862bafb8c6a2a298b397980149354e
mariaaliceas <mariaalicearistides29@gmail.com>
2024-03-03 16:13:19 -0300

    Adicionando funções

--- pdfReader.py
+++ pdfReader.py
@@ -28,40 +28,46 @@ bag_of_words = []
 most_commons = []
 reference = False
 
-for i, word in enumerate(words):
-    if (word == 'r' and words[i+1] == 'eferences') or (word == 'references' and words[i+1] == '1'):
-        reference = True
-    if reference:
-        if word == 'authorized':
-            references.pop()
-            break
-        references.append(word.lower())
-    else:
-        texto.append(word.lower())
-
-if not tratar_texto:
-    True
-else:
-    stop_words = stopwords.words('english')
-
-    for word in words:
+def remove_references(reference):
+    for i, word in enumerate(words):
         if (word == 'r' and words[i+1] == 'eferences') or (word == 'references' and words[i+1] == '1'):
-                reference = True
-        if (word not in stop_words) and not reference:
-            texto.append(word)
-        elif (word not in stop_words) and reference:
+            reference = True
+        if reference:
             if word == 'authorized':
                 references.pop()
                 break
-            references.append(word)
+            references.append(word.lower())
+        else:
+            texto.append(word.lower())
 
-    porter = nltk.PorterStemmer()
-    stemm_words = [porter.stem(w) for w in texto]
-    # print(stemm_words)
+def text_treatment(word):
+    reference = []
+    if not tratar_texto:
+        True
+    else:
+        stop_words = stopwords.words('english')
+
+        for word in words:
+            if (word == 'r' and words[i+1] == 'eferences') or (word == 'references' and words[i+1] == '1'):
+                    reference = True
+            if (word not in stop_words) and not reference:
+                texto.append(word)
+            elif (word not in stop_words) and reference:
+                if word == 'authorized':
+                    references.pop()
+                    break
+                references.append(word)
+
+        porter = nltk.PorterStemmer()
+        stemm_words = [porter.stem(w) for w in texto]
+        # print(stemm_words)
 
 bag_of_words = Counter(texto)
 most_commons = bag_of_words.most_common(10)
-print(texto)
+remove_references(words)
+text_treatment(words)
+print(words)
+
 #print(references)
 #print(bag_of_words)
 #print(most_commons)
